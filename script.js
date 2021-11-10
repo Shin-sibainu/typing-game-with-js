352f045b56c1d4fc146f64ed85259c858431c659
Shin-sibainu <shincode0712@gmail.com>
2021-11-10 12:08:12 +0900

    all done. but modified something

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const RANDOM_SENTENCE_URL_API = "http://api.quotable.io/random";
 const typeDisplayElement = document.getElementById("typeDisplay");
 const typeInputElement = document.getElementById("typeInput");
+const timer = document.getElementById("timer");
+
+/* inputテキスト入力。合っているかどうかの判定 */
+typeInputElement.addEventListener("input", () => {
+  /* 文字と文字を比較する */
+  /* ディスプレイに表示されてるSpanタグを取得 */
+  const sentence = typeDisplayElement.querySelectorAll("span");
+  /* 自分で打ち込んだテキストを取得 */
+  const arrayValue = typeInputElement.value.split("");
+  let correct = true;
+  sentence.forEach((characterSpan, index) => {
+    if (arrayValue[index] == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (characterSpan.innerText == arrayValue[index]) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.add("incorrect");
+      characterSpan.classList.remove("correct");
+      correct = false;
+    }
+  });
+
+  if (correct) RenderNextSentence();
+});
 
 /* ちゃんとthenかawaitで待たないと欲しいデータが入らない。 */
 /* 非同期でランダムな文章を取得する */
@@ -33,6 +60,23 @@ async function RenderNextSentence() {
   });
   /* テキストボックスの中身を消す。 */
   typeInputElement.value = null;
+
+  /* タイマーのリセット */
+  StartTimer();
+}
+
+let startTime;
+/* カウントアップを開始する。 */
+function StartTimer() {
+  timer.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
 }
 
 RenderNextSentence();
